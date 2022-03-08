66a17208f54bfe899f3864654af991383f449428
Javier Minotta <jaminotta@indracompany.com>
2022-03-08 12:56:25 -0500

    Deploy

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -13,6 +13,7 @@ from six import python_2_unicode_compatible
 import django.utils.encoding
 django.utils.encoding.python_2_unicode_compatible = python_2_unicode_compatible
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ivkk4^705sw0g@pzu$x3_&l89*vl9v-06f9t+_n+d4t+q4n4#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,6 +131,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -137,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
