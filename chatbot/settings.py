4e51c7e7a6ef9e46ea06c48bab4fe64ded9f356c
Javier Minotta <jaminotta@indracompany.com>
2022-03-09 02:43:16 -0500

    Deploy V7 Pre - Variables entorno

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
+DEBUG = config('DJANGO_DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
